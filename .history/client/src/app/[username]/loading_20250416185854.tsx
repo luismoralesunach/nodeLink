04aeae27a3f1e9ddcdc76035bd8b2e0358d5bcf5
luismoralesunach <luismorales2914@gmail.com>
2025-04-16 18:59:27 -0600

    small change

---
+++ .history/client/src/app/[username]/loading_20250416185854.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Loading() {
+
+    return(
+        <div className='flex flex-row justify-center items-center'>
+            <p className="text-gray-500">Loading lol...</p>
+        </div>
+    )
+}
